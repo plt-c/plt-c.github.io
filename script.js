1ab0bf43ab151660257ad8c6e659b72b1ff58eac
plt-c <polatcaliskan4@gmail.com>
2026-05-08 16:24:10 +0200

    Simplify theme toggle event listener setup

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const toggle = document.getElementById("themeToggle");
 
-if (toggle) {
-    toggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-    });
-}
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
