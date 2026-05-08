4cfcb20eb92ae7cddc4ed1f9c5a35f90c718b701
plt-c <polatcaliskan4@gmail.com>
2026-05-08 16:16:44 +0200

    Add null check for theme toggle element

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggle = document.getElementById("themeToggle");
 
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-});
+if (toggle) {
+    toggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+    });
+}
