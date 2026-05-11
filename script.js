bfe799d97c0db4abc523a6764b73e2e7015a2321
plt-c <polatcaliskan4@gmail.com>
2026-05-11 13:14:01 +0200

    Add rebuild marker in script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // =========================================
 // SIMPLE NAV ACTIVE STATE
 // =========================================
+<-----------rebuild-------------->
 
 (function () {
   const path = window.location.pathname.toLowerCase();
